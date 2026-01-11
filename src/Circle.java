b52f52623ef36ee2fe5d8163a77e085d9f5dc6d4
Quinn Pucci <pucciquinn@gmail.com>
2026-01-11 15:38:36 -0700

    completed methods for circle class and added some commenting

--- src/Circle.java
+++ src/Circle.java
@@ -1,36 +1,37 @@
 public class Circle {
 
+    // Declare the attributes
     double xPoint;
     double yPoint;
     double radius;
 
+    // Default constructor - hardcoded data
     public Circle() {
         radius = 1.0;
         xPoint = 0.0;
         yPoint = 0.0;
     }
 
+    // Constructor - data as input
     public Circle(double xPoint, double yPoint, double radius){
         this.xPoint = xPoint;
         this.yPoint = yPoint;
-        this.radius = radius;
-
-        // setRadius(radius)
+        setRadius(radius); // use set radius function to set the radius
     }
 
     public double circumference(){
-        double circumference = 2 * Math.PI * this.radius;
-        return circumference;
+        return 2 * Math.PI * this.radius;
+        // Make use of Math class PI method to achieve formula for circumference
     }
 
     public double area(){
-        double area = Math.PI * (Math.pow(this.radius, 2));
-        return area;
+        return Math.PI * (Math.pow(this.radius, 2));
+        // makes use of Math classes pi method and power method for area of a circle formula
     }
 
     public void setRadius(double r){
-        // is called in the constructor and checks the radius against a maximum. If the radius is greater than the maximum,
-        // setRadius resets it to the maximum (using the ternary conditional operator). You may set your own maximum value.
+        this.radius = (r <= 5) ? r : 5;
+        // uses ternary conditional operator to set radius attribute as
     }
 
     public void printAttributes() {
@@ -40,9 +41,11 @@ public class Circle {
     }
 
     public boolean isInside(double x, double y){
+        // equation REF: https://www.intellectualmath.com/determine-if-a-point-lies-inside-or-outside-of-a-circle.html
+        if (Math.pow(x - this.xPoint, 2) + Math.pow(y - this.yPoint, 2) < Math.pow(this.radius, 2)) {
+            return true;
+        } else { return false; }
 
-        // return true if a point represented in the parameters falls inside the circle, false otherwise
-        return true;
     }
 
     public void move(int x, int y){
@@ -54,6 +57,14 @@ public class Circle {
         Circle circle1 = new Circle();
         System.out.println(circle1.area());
         System.out.println(circle1.circumference());
+        circle1.printAttributes();
+        Circle circle2 = new Circle(4.0, -5.0, 7.0);
+        System.out.println(circle2.area());
+        System.out.println(circle2.circumference());
+        circle2.printAttributes();
+        System.out.println(circle2.isInside(7, -6));
+        System.out.println(circle2.isInside(-85,90));
+        circle1.move(-3, 3);
         circle1.printAttributes();
     }
 
