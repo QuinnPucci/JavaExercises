fd7987e58fd753000aa59598344534a8865e5309
Quinn Pucci <pucciquinn@gmail.com>
2026-03-07 11:48:43 -0700

    did a simple generic container exercise with its testing

--- src/Box.java
+++ src/Box.java
@@ -30,25 +30,28 @@ public class Box<T> {
         newBox.addItem("Cat");
         newBox.addItem(5.67879);
         newBox.addItem(4);
-        newBox.getAllItems();
-        newBox.size();
+
+        System.out.println(newBox.getAllItems());
+        System.out.println(newBox.size());
 
         Box<Integer> intBox = new Box<>(); // a box that only holds ints
         intBox.addItem(5);
         intBox.addItem(6);
         intBox.addItem(42);
 
-        intBox.getItem(1);
-        intBox.getAllItems();
-        intBox.size();
+        System.out.println("");
+        System.out.println(intBox.getItem(1));
+        System.out.println(intBox.getAllItems());
+        System.out.println(intBox.size());
 
         Box<String> stringBox = new Box<>(); // a box that only hold string
         stringBox.addItem("cats");
         stringBox.addItem("dogs");
         stringBox.addItem("foxes");
 
-        stringBox.getItem(0);
-        stringBox.getAllItems();
-        stringBox.size();
+        System.out.println("");
+        System.out.println(stringBox.getItem(0));
+        System.out.println(stringBox.getAllItems());
+        System.out.println(stringBox.size());
     }
 }
